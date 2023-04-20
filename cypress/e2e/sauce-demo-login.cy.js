b4566238a74e6cc0f2daee039f0d6486a1729df4
nzobic <zobicnatasa@gmail.com>
2023-04-20 12:27:17 +0200

    Fixed Login Test

--- cypress/e2e/sauce-demo-login.cy.js
+++ cypress/e2e/sauce-demo-login.cy.js
@@ -4,13 +4,15 @@ it('Login', () => {
     
     cy.fixture('sauce-demo-login.json').then((data) => {
 
-//Try to login with different credentials
+//Login
     
     cy.visit('https://www.saucedemo.com/')
-
-    for (let i = 0; i<data.credentials.length; i++) {
+    
+    for (let i = 0; i<data.credentials.length; i++) {     
         cy.get('#user-name').eq(i).type(data.credentials[i].username)
         cy.get('#password').eq(i).type(data.credentials[i].password)
+        cy.get('#login-button').click()
+
         cy.go('back')
     }
 
