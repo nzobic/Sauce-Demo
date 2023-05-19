3cd6bf7c8bc7fff7f230276b0fde32824ba53da5
nzobic <zobicnatasa@gmail.com>
2023-05-19 11:05:07 +0200

    Updates

--- cypress/e2e/sauce-demo-login.cy.js
+++ cypress/e2e/sauce-demo-login.cy.js
@@ -1,30 +1,25 @@
 describe('Sauce-Demo Test', () => {
-  
-it('Login', () => {
+
+    it('Login', () => {
+
+        cy.fixture('sauce-demo-login.json').then((data) => {
+
+        //Login
+        cy.visit('https://www.saucedemo.com/')
+
+        // cy.get('#user-name').eq(0).type(data.userData[i].username)
+
+        for (let i = 0; i < data.userData.length; i++) {
+            cy.get('#user-name').type(data.userData[i].username)
+            cy.get('#password').type(data.userData[i].password)
+            cy.get('#login-button').click()
+
+            cy.url().should('contain', '/inventory')
+            cy.go('back')
+        }
     
-    cy.fixture('sauce-demo-login.json').then((data) => {
-
-//Login
+        })
     
-    cy.visit('https://www.saucedemo.com/')
-
-   // cy.get('#user-name').eq(0).type(data.userData[i].username)
-
-    
-    for (let i = 0; i<data.userData.length; i++) {     
-        cy.get('#user-name').type(data.userData[i].username)
-        cy.get('#password').type(data.userData[i].password)
-        cy.get('#login-button').click()
-
-        cy.url().should('contain', '/inventory')
-
-        cy.go('back')
-    }
-
     })
-
-    
-
-})
 
 })
